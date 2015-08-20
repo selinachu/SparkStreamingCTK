5d673f38aa37873c51fec722a79f79fefae95abd
selina <selina.chu@gmail.com>
2015-08-19 17:57:59 -0700

    remove target

--- target/spark-ctakes-0.1/bin/start-ctakes-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [[ $# != 1 ]]
-then
-    echo "Usage:\n\t${0} <port>\n\tStarts up the ctakes streaming server." 1>&2
-    exit 1
-fi
-CTAKES_HOME=/usr/local/apache-ctakes-3.2.2/
-CP="${CTAKES_HOME}/desc/:${CTAKES_HOME}/resources/:$( find $(dirname $0)/../lib ${CTAKES_HOME}/lib  -name "*.jar" | tr '\n' ':')"
-${JAVA_HOME}/bin/java -cp ${CP} org.dia.red.ctakes.spark.CtakesSparkMain ${1}
